908f7b33899978c1dfe1823705a861579ff7954c
Bobby0089 <161326642+Bobby0089@users.noreply.github.com>
2025-02-25 20:17:25 +0530

    Add files via upload

---
+++ MovieApp/MoviesApp.java
@@ -0,0 +1,13 @@
+package com.company.Day9_HW;
+
+import java.io.IOException;
+
+public class MoviesApp {
+
+	public static void main(String[] args) throws IOException {
+		MovieController movieController= new MovieController();
+		movieController.start();
+
+	}
+
+}
